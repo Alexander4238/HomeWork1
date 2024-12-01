0daf5c5d099237864131059f159a9035f9f89efb
Alexander4238 <sadw403@gmail.com>
2024-12-01 22:17:52 +0700

    add the final version MyArrayList and MyArrayListTest

--- src/test/java/org/example/MyArrayListTest.java
+++ src/test/java/org/example/MyArrayListTest.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.junit.Test;
+import java.util.Arrays;
 import static org.junit.Assert.*;
 
 public class MyArrayListTest {
@@ -8,11 +9,12 @@ public class MyArrayListTest {
     @Test
     public void testAdd() {
         MyArrayList<Integer> list = new MyArrayList<>();
-        list.add(1);
-        list.add(2);
-        assertEquals(2, list.size());
-        assertEquals(Integer.valueOf(1), list.get(0));
-        assertEquals(Integer.valueOf(2), list.get(1));
+        for(int i = 0; i < 1000; i++){
+            list.add(i);
+        }
+        assertEquals(1000, list.size());
+        assertEquals(Integer.valueOf(0),list.get(0));
+        assertEquals(Integer.valueOf(999),list.get(999));
     }
 
     @Test
@@ -20,11 +22,10 @@ public class MyArrayListTest {
         MyArrayList<Integer> list = new MyArrayList<>();
         list.add(1);
         list.add(2);
-        list.add(1, 3); //Добавление по индексу
+        list.add(1, 3);
         assertEquals(3, list.size());
         assertEquals(Integer.valueOf(1), list.get(0));
-        assertEquals(Integer.valueOf(3), list.get(1));
-        assertEquals(Integer.valueOf(2), list.get(2));
+                assertEquals(Integer.valueOf(2), list.get(2));
     }
 
     @Test
@@ -36,13 +37,24 @@ public class MyArrayListTest {
         list.remove(1);
         assertEquals(2, list.size());
         assertEquals(Integer.valueOf(1), list.get(0));
-        assertEquals(Integer.valueOf(3), list.get(1));
     }
 
     @Test(expected = IndexOutOfBoundsException.class)
-    public void testIndexOutOfBoundsException() {
+    public void testIndexOutOfBoundsExceptionGet() {
         MyArrayList<Integer> list = new MyArrayList<>();
         list.get(0);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testIndexOutOfBoundsExceptionRemove() {
+        MyArrayList<Integer> list = new MyArrayList<>();
+        list.remove(0);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testIndexOutOfBoundsExceptionAdd() {
+        MyArrayList<Integer> list = new MyArrayList<>();
+        list.add(1,1);
     }
 
     @Test
@@ -55,14 +67,29 @@ public class MyArrayListTest {
     }
 
     @Test
-    public void testSort(){
+    public void testSortComparable() {
         MyArrayList<Integer> list = new MyArrayList<>();
         list.add(3);
         list.add(1);
         list.add(2);
         list.sort();
-        assertEquals(Integer.valueOf(1), list.get(0));
-        assertEquals(Integer.valueOf(2), list.get(1));
-        assertEquals(Integer.valueOf(3), list.get(2));
+        assertArrayEquals(new Integer[]{1, 2, 3}, Arrays.copyOfRange(list.array,0,list.size()));
+    }
+    @Test
+    public void testSet(){
+        MyArrayList<Integer> list = new MyArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.set(0,3);
+        assertEquals(Integer.valueOf(3), list.get(0));
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void testSortWithoutComparable(){
+        MyArrayList<Object> list = new MyArrayList<>();
+        list.add(new Object());
+        list.add(new Object());
+        list.sort();
+
     }
 }
